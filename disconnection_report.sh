5780a397ab091043408760dbfd5b22342d3c6ffa
tea551234 <tea551234@gmail.com>
2023-01-05 15:07:23 +0800

    202301051502 acmqtt

--- disconnection_report.sh
+++ disconnection_report.sh
@@ -2,16 +2,19 @@
 #2022_11_27 coding by willis  
 #2023_01_04 coding by teaya  
 
-#需定義iptable / pwd,ip,id
-file="./iptable.csv"  
+
+file="./iptable.csv"   #需定義iptable / pwd,ip,id
+useKACMQTT="1"    #使用kill mqtt 使用:1 關閉:0
 
 for line in $(cat $file)
     do
         pwd=$(echo $line | cut -d"," -f 1)
 	    	ip=$(echo $line | cut -d"," -f 2)
 	    	id=$(echo $line | cut -d"," -f 3)
-        # checkDate=$(echo $(date -d "yesterday" '+%F'))
         sshPass="sshpass -p "$pwd" ssh -o PasswordAuthentication=yes moxa@"$ip" " #ssh連線 
+        log_file="/home/moxa/killmqtt.log"
+        # checkDate=$(echo $(date -d "yesterday" '+%F'))
+        
         mdtime=$(stat ka_diag.log | awk '/Modify/ {print $2}') 
         checkDate=$(date -d "yesterday" '+%F') 
        # checkDate='2022-12-30'
@@ -19,7 +22,23 @@ for line in $(cat $file)
       count=$(ping -c 2 $ip | grep from* | wc -l)
       if [ $count -gt 0 ]; then
 ##確認mqtt
+      c_mqtt=$($sshPass ps -eo cmd,pid,%mem | grep ACMqtt | grep i | sed 's/\s+/ /g' )
+      mqttPid=$( echo $c_mqtt | awk '{print $4}' ) #PID
+      mqttMEM=$( echo $c_mqtt | awk '{print $5}' | awk -F"." '{print $1}') #mem使用量
+      # mqttMEM="6" #mem使用量
+      while true; do
+        if [ $mqttMEM -lt 5 ] || [ $useKACMQTT == "0" ];then
+          echo "小於 5% "
+          break  # 跳出當前回圈
+        else
+          echo "大於 5%"
+          break   # 跳出整個迴圈
+        fi
+      done
 
+
+      # echo $mqttPid
+      # echo $mqttMEM
 
 ##確認時間
 
