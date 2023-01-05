6864d4a29c0a3a6794347a7172bf8bcd11310777
tea551234 <tea551234@gmail.com>
2023-01-05 12:59:51 +0800

    202301051300 基本check功能OK

--- disconnection_report.sh
+++ disconnection_report.sh
@@ -2,10 +2,6 @@
 #2022_11_27 coding by willis  
 #2023_01_04 coding by teaya  
 
-
-
-
-
 #需定義iptable / pwd,ip,id
 file="./iptable.csv"  
 
@@ -17,44 +13,29 @@ for line in $(cat $file)
         # checkDate=$(echo $(date -d "yesterday" '+%F'))
         sshPass="sshpass -p "$pwd" ssh -o PasswordAuthentication=yes moxa@"$ip" " #ssh連線 
         mdtime=$(stat ka_diag.log | awk '/Modify/ {print $2}') 
-       checkDate=$(date -d "yesterday" '+%F') 
+        checkDate=$(date -d "yesterday" '+%F') 
        # checkDate='2022-12-30'
+
+      count=$(ping -c 2 $ip | grep from* | wc -l)
+      if [ $count -gt 0 ]; then
+##確認mqtt
+
+
+##確認時間
+
+
+
+## 確認PINGNG
         if [   $mdtime == $checkDate  ];then
-        getSnmac=$( echo "moxa" | $sshPass sudo -S fw_printenv 2>&1 | awk -F'=' '/(serialnumber|ethaddr)/ {printf "%s,", $2}')  # get SN/MAC 
-        getIp=$( $sshPass ip a | awk '/inet [0-9]{3}\.[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{2,3}/ {gsub("/[0-9]+$", "", $2); print $2}' )  # get SN/MAC 
-        getPingng="$sshPass cat /home/moxa/ka_diag.log | awk '/$checkDate.*PING/ {print \$1,\$2}'"
+          getSnmac=$( echo "$pwd" | $sshPass sudo -S fw_printenv 2>&1 | awk -F'=' '/(serialnumber|ethaddr)/ {printf "%s,", $2}')  # get SN/MAC 
+          getPingng="$sshPass cat /home/moxa/ka_diag.log | awk '/$checkDate.*PING/ {print \$1,\$2}'"
         # $getPingng
-        echo $($getPingng) , $getSnmac $getIp,"PING NG"  >> /home/moxa/fwprintenv.csv #NGTIME/SN,MAC/IP
-        else
-        echo $checkDate "," $ip "," "SF"  >> /home/moxa/fwprintenv.csv # 無斷線
+          echo $($getPingng) , $getSnmac $ip,"PING NG"  >> /home/moxa/"$(date +%Y%m).csv" #NGTIME/SN,MAC/IP
+       else
+          echo $checkDate "," $ip "," "SF"  >> /home/moxa/"$(date +%Y%m).csv" # 無斷線
         fi
-   
+      else
+          echo $checkDate","$ip",-1" >> ./"$(date +%Y%m).csv"
+      fi
     done
 echo "finish"
-
-
-
-
-
-        # if [ $count -gt 0 ]; then
-      	# #p_checkCount="$(sshpass -p "$pwd" ssh -o stricthostkeychecking=no $cmd )"
-      	# #echo "$(date +%F)"","$ip","$p_checkCount >> ./"$(date +%Y%m).csv"
-        # 	echo $checkDate","$ip","$p_checkCount >> ./"$(date +%Y%m).csv"
-        # else
-      	# #echo "$(date +%F)"","$ip",-1" >> ./"$(date +%Y%m).csv"
-      	#     echo $checkDate","$ip",-1" >> ./"$(date +%Y%m).csv"
-        #  fi   
-
-# mdTime="2023-01-03"
-# echo $checkDate
-
-
-
-
-
-
-
-
-
-
-
